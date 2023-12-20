246ae83a551cc5281ebb40e136b7b1f3651e1560
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-20 16:42:43 +0100

    add: ex 7 and 8

--- queries.sql
+++ queries.sql
@@ -1,24 +1,26 @@
 -- 1. Select all students born in 1990
 SELECT * FROM `students`
-WHERE `date_of_birth` LIKE '1990%'
-LIMIT 160;
+WHERE `date_of_birth` LIKE '1990%';
 
 -- 2. Select all courses which cfu is more than 10
 SELECT * FROM `courses`
-WHERE `cfu` > 10
-LIMIT 479;
+WHERE `cfu` > 10;
 
 -- 4. Select all courses of the first semester of the first year
 SELECT * FROM `courses`
-WHERE `year` = 1 AND `period` LIKE 'I semestre'
-LIMIT 286;
+WHERE `year` = 1 AND `period` LIKE 'I semestre';
 
 -- 5. Select all exams that take place in the afternoon of 2020/06/20
 SELECT * FROM `exams`
-WHERE HOUR(`hour`) = 14 AND DATE(`date`) = '2020-06-20'
-LIMIT 21;
+WHERE HOUR(`hour`) = 14 AND DATE(`date`) = '2020-06-20';
 
 -- 6. Select all master's degrees
 SELECT * FROM `degrees`
-WHERE `name` LIKE '%Magistrale%'
-LIMIT 38;
+WHERE `name` LIKE '%Magistrale%';
+
+-- 7. Count all departments
+SELECT COUNT(*) FROM `departments`;
+
+-- 8. Count all teachers without a phone number
+SELECT COUNT(*) FROM `teachers`
+WHERE `phone` IS NULL;
