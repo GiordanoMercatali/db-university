413888793dfee95fc972b338447f8de0ded299aa
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-21 18:30:54 +0100

    add: ex 6 join

--- queries_join.sql
+++ queries_join.sql
@@ -29,3 +29,11 @@ SELECT * FROM `degrees`
 INNER JOIN `courses` ON `courses`.`degree_id` = `degrees`.`id`
 JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
 JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`;
+
+-- 6. Select all teachers who teach in Math department
+SELECT * FROM `teachers`
+INNER JOIN `course_teacher` ON `course_teacher`.`teacher_id` = `teachers`.`id`
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees` ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
