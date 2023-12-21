ccf703573f7f39fcf49532e0d9ded6b11285ea58
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-21 15:19:33 +0100

    add: ex2

--- queries_groupby.sql
+++ queries_groupby.sql
@@ -2,3 +2,8 @@
 SELECT COUNT(*) as `students_of_year`, YEAR(`enrolment_date`) as `year_of_enrolment`
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Count how many teachers have their office at the same address
+SELECT COUNT(*) as `offices_at_address`, `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
