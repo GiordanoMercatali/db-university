b8807c888486a4b4352e250adca69cb9d9fd8ec2
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-21 15:38:26 +0100

    add: ex 3

--- queries_groupby.sql
+++ queries_groupby.sql
@@ -7,3 +7,9 @@ GROUP BY YEAR(`enrolment_date`);
 SELECT COUNT(*) as `offices_at_address`, `office_address`
 FROM `teachers`
 GROUP BY `office_address`;
+
+-- 3. Average of votes for each exam
+SELECT `exam_id`, AVG(`vote`) as `votes_average`
+FROM `exam_student`
+-- WHERE `vote` >= 18 AND `vote` <= 30 in case we wanted to count only the students who passed the exam
+GROUP BY `exam_id`;
