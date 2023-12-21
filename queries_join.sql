069f4f18d61214aceb4cedf58e8e94fefbf7b0cc
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-21 17:48:31 +0100

    add: ex 4 join

--- queries_join.sql
+++ queries_join.sql
@@ -17,3 +17,9 @@ SELECT * FROM `courses`
 INNER JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
 INNER JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
 WHERE `teachers`.`name` = 'Fulvio' AND `teachers`.`surname` = 'Amato';
+
+-- 4. Select all students with all the info on their degree and degree's department, in alphabetical order by surname and name 
+SELECT * FROM `students`
+INNER JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id`
+INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC, `students`.`name` ASC;
