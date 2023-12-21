ece9bcd9abe25043fcf087c1b4ecdcce2356be40
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-21 17:12:50 +0100

    add: join ex 2

--- queries_join.sql
+++ queries_join.sql
@@ -2,3 +2,13 @@
 SELECT * FROM `students`
 INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
 WHERE  `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Select all Master's Degrees in Neuroscience Department
+SELECT * FROM `degrees`
+INNER JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+WHERE  `departments`.`name` = 'Dipartimento di Neuroscienze' AND `degrees`.`level` = 'Magistrale';
+
+-- SELECT * FROM `departments`
+-- INNER JOIN `degrees` ON `degrees`.`department_id` = `departments`.`id`
+-- WHERE  `departments`.`name` = 'Dipartimento di Neuroscienze' AND `degrees`.`level` = 'Magistrale';
+
